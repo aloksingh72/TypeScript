b0ee202ecc4b94620e5caf8b16346efbd06bd0d4
Alok Singh <aloksingh36757@gmail.com>
2024-08-16 17:47:34 +0530

    learning typescript

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("hello Typescript");
+// @ts-ignore
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 5));
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("hello Typescript");
+// @ts-ignore
+
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+console.log(sum(1, 5));
