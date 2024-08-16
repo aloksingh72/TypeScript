d9a2ecf5dc0e8eabbb905662c848fa5e90cebe64
Alok Singh <aloksingh36757@gmail.com>
2024-08-16 21:58:27 +0530

    Types completed and Functions started

--- Types/type_annotations.js
+++ Types/type_annotations.js
@@ -1,4 +1,5 @@
 "use strict";
+// Dated-->16/Aug/2024
 let message = "hello Typescript";
 console.log(message);
 // decalre a variable and assign a sentence to it and find its length
@@ -14,3 +15,23 @@ console.log(upperLine);
 let longText = "hello this is longtext is used for the filter the substring the 10 characters form the long text. ";
 let shortText = longText.substring(0, 10);
 console.log(shortText);
+//  todo   String Comparison: 
+// Declare two variables str1 and str2 of type string and assign them different sentences. Compare the two strings and store the result (true or false) in a variable called areEqual.
+let str1 = "aloksingh36757@gmail.com";
+let str2 = "aloksingh36757@gmail.com";
+let areEqual = str1 === str2;
+console.log(areEqual);
+// Declare variables product and price of type string and number, respectively. Create a string using template literals to display the product and its price in the format "The product {product} is priced at {price} dollars."
+let product = "Oreo Biscuit";
+let price = 10;
+console.log(`The product ${product} is priced at ${price} dollars.`);
+// TODO: make  a funclion isEven that take a number as perimeter and return true if the number is true and false otherwise
+function isEven(a) {
+    return a % 2 === 0;
+}
+console.log(isEven(9));
+// TODO : write a typescript function isDivisibleBy4and8 that takes a number as a parameter and returns true if the number is divisible by both 4 and 8.
+function isDivisibleBy4and8(x) {
+    return x % 4 === 0 && x % 8 === 0;
+}
+console.log(isDivisibleBy4and8(8));
--- Types/type_annotations.ts
+++ Types/type_annotations.ts
@@ -1,3 +1,5 @@
+// Dated-->16/Aug/2024
+
 let message: string= "hello Typescript"
 console.log(message);
 
@@ -15,4 +17,30 @@ console.log(upperLine);
 // it a long sentence. Extract the first 10 characters from longText and store them in a variable called shortText. 
 let longText:String="hello this is longtext is used for the filter the substring the 10 characters form the long text. "
 let shortText:String=longText.substring(0,10);
-console.log(shortText); 
+console.log(shortText);
+
+//  todo   String Comparison: 
+// Declare two variables str1 and str2 of type string and assign them different sentences. Compare the two strings and store the result (true or false) in a variable called areEqual.
+let str1:string ="aloksingh36757@gmail.com";
+let str2:string ="aloksingh36757@gmail.com";
+let areEqual:boolean = str1=== str2;
+console.log(areEqual);
+
+// Declare variables product and price of type string and number, respectively. Create a string using template literals to display the product and its price in the format "The product {product} is priced at {price} dollars."
+let product:string ="Oreo Biscuit";
+let price:number = 10;
+console.log(`The product ${product} is priced at ${price} dollars.`);
+
+// TODO: make  a funclion isEven that take a number as perimeter and return true if the number is true and false otherwise
+
+function isEven(a:number):boolean{
+    return a%2 === 0
+
+}
+console.log(isEven(9));
+// TODO : write a typescript function isDivisibleBy4and8 that takes a number as a parameter and returns true if the number is divisible by both 4 and 8.
+
+function isDivisibleBy4and8(x:number):boolean{
+return x%4===0 && x%8===0 
+}
+console.log(isDivisibleBy4and8(8));
