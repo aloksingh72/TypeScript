dcecbc2de882bda97b7d388c562d388db47083fa
Alok Singh <aloksingh36757@gmail.com>
2024-08-16 18:29:18 +0530

    typeannotions

---
+++ Types/type_annotations.js
@@ -0,0 +1,16 @@
+"use strict";
+let message = "hello Typescript";
+console.log(message);
+// decalre a variable and assign a sentence to it and find its length
+let sentence = "hello Typescript";
+let sentenceLength = sentence.length;
+console.log(sentenceLength);
+//convert the given string into the upper case
+let line = "my name is aloksingh";
+let upperLine = line.toUpperCase();
+console.log(upperLine);
+//Declare a variable longText of type string and assign 
+// it a long sentence. Extract the first 10 characters from longText and store them in a variable called shortText. 
+let longText = "hello this is longtext is used for the filter the substring the 10 characters form the long text. ";
+let shortText = longText.substring(0, 10);
+console.log(shortText);
---
+++ Types/type_annotations.ts
@@ -0,0 +1,18 @@
+let message: string= "hello Typescript"
+console.log(message);
+
+// decalre a variable and assign a sentence to it and find its length
+let sentence:string = "hello Typescript"
+let sentenceLength:number = sentence.length;
+console.log(sentenceLength);
+
+//convert the given string into the upper case
+let line:string = "my name is aloksingh";
+let upperLine:string = line.toUpperCase();
+console.log(upperLine); 
+
+//Declare a variable longText of type string and assign 
+// it a long sentence. Extract the first 10 characters from longText and store them in a variable called shortText. 
+let longText:String="hello this is longtext is used for the filter the substring the 10 characters form the long text. "
+let shortText:String=longText.substring(0,10);
+console.log(shortText); 
